50c2d776468245be1e5f034aa869c062fd62ffbb
EdwinDC-ux <edwin9602@gmail.com>
2021-01-31 14:19:13 -0600

    Se crea la funcion 'sendData' encargada de mandar 'POST' los datos para agregar un nuevo carro

--- assets/js/validateSubmit.js
+++ assets/js/validateSubmit.js
@@ -1,4 +1,50 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
+const sendData = () => {
+    const $imagen = $("#imageURL")
+    const $marca = $("#marca")
+    const $subMarca = $("#submarca")
+    const $id = $("#ident")
+    const $kmActual = $("#kmAct")
+    const $fechaProgr = $("#programada")
+    const $descripcion = $("#descripcion")
+    const $fechaEstim = $("#estimada")
+    const $encargado = $("#encargado")
+    const dataSend = {
+        image: $imagen.val(),
+        make: $marca.val(),
+        model: $subMarca.val(),
+        id: $id.val(),
+        km: $kmActual.val(),
+        estimateDate: $fechaProgr.val(),
+        description: $descripcion.val(),
+        estimateDeliveryDate: $fechaEstim.val(),
+        personName: $encargado.val()
+    }
+
+
+    $.ajax({
+        type: "POST",
+        url: "https://ecb-server.arianaomi.vercel.app/cars",
+        data: JSON.stringify(dataSend),
+        error: e => {
+            console.log(e)
+        },
+        success: () => {
+            $imagen.val("")
+            $marca.val("")
+            $subMarca.val("")
+            $id.val("")
+            $kmActual.val("")
+            $fechaProgr.val("")
+            $descripcion.val("")
+            $fechaEstim.val("")
+            $encargado.val("")
+        },
+        dataType: "json",
+        contentType: "application/json"
+    })
+}
+
 (function () {
     'use strict'
 
@@ -12,9 +58,13 @@
                 if (!form.checkValidity()) {
                     event.preventDefault()
                     event.stopPropagation()
+                    form.classList.add('was-validated')
+                } else {
+                    event.preventDefault()
+                    event.stopPropagation()
+                    sendData()
+                    $("form").removeClass("was-validated")
                 }
-
-                form.classList.add('was-validated')
             }, false)
         })
 })()
