8d5867d44ff50e8626197dcd417ccbfecb38672f
EdwinDC-ux <edwin9602@gmail.com>
2021-02-01 14:24:05 -0600

    Reestructura unos elementos y adicion del formulario y boton

--- assets/js/receiveData.js
+++ assets/js/receiveData.js
@@ -2,36 +2,54 @@
 
 const printData = car => {
 
-    const $image = $(`<img src="${car.image}" class="img-fluid"></img>`)
-    const $make = $(`<p class="make">Marca: ${car.make}</p>`)
-    const $model = $(`<p class="model">Modelo: ${car.model}</p>`)
-    const $id = $(`<p class="identif">Id: ${car.id}</p>`)
-    const $km = $(`<p class="km">Kilometros Recorridos: ${car.km}</p>`)
-    const $dateIn = $(`<p class="dateIn">Fecha Programada: ${car.estimateDate}</p>`)
-    const $descr = $(`<p class="descr">Descripción: ${car.description}</p>`)
-    const $dateOut = $(`<p class="dateOut">Fecha estimada de entrega: ${car.estimateDeliveryDate}</p>`)
-    const $personName = $(`<p class="personName">Encargado: ${car.personName}</p>`)
-    const $articleCont = $(`<div class="col-md-12 text-center artCont"></div>`)
-    const $imgCont = $(`<div class="col-md-6 imgCont"></div>`)
-    const $rowCont = $(`<div class="row justify-content-center"></div>`)
-    const $colCont = $(`<div class="col-md-5 item"></div>`)
-    const $container = $("#articleCont")
-    const $buttonUpDate = $('<a>Actualizar fecha de entrega<a>')
+    const $container = $("#articleCont"),
+        $colCont = $(`<div class="col-md-5 item"></div>`),
+        $rowCont = $(`<div class="row justify-content-center"></div>`),
+        $imgCont = $(
+            `<div class="col-md-6 imgCont">
+            <img src="${car.image}" class="img-fluid"></img>
+        </div>`
+        ),
+        $articleCont = $(
+            `<div class="col-md-12 text-center artCont">
+                <p class="make">Marca: ${car.make}</p>
+                <p class="model">Modelo: ${car.model}</p>
+                <p class="identif">Id: ${car.id}</p>
+                <p class="km">Kilometros Recorridos: ${car.km}</p>
+                <p class="dateIn">Fecha Programada: ${car.estimateDate}</p>
+                <p class="descr">Descripción: ${car.description}</p>
+                <p class="dateOut">Fecha estimada de entrega: ${car.estimateDeliveryDate}</p>
+                <p class="personName">Encargado: ${car.personName}</p>
+            </div>`
+        ),
+        $formCont = $(
+            `<div class="col-md-12 formCont">
+                <form class"row g-3">
+                    <div class="col-md-12 elementFormCont">
+                        <label for="estimada" class="form-label">Fecha estimada de entrega</label>
+                        <input type="date" class="form-control" name="estimateDeliveryDate" id="estimada">
+                    </div>
+                    <div class="col-md-12 elementFormCont">
+                        <label for="encargado" class="form-label">Persona encargada</label>
+                        <input type="text" class="form-control" id="encargado" name="personName">
+                    </div>
+                    <div class="col-md-12 elementFormCont">
+                        <input type="hidden" class="form-control" id="id" name="gId" value="${car._id}">
+                    </div>
+                    <div class="col-md-12 hidden text-center">
+                        <button class="btn btn-primary" type="submit" id="agregar">Agregar servicio</button>
+                        <button class="btn btn-primary" type="submit" id="borrar">Quitar servicio</button>
+                    </div>
+                </form>
+            </div>`
+        ),
+        $button = $(
+            `<div class="col-md-12 buttonCont text-center">
+                <button class="btn btn-primary" type="submit" id="desplegar">Agregar un servicio</button>
+            </div>`
+        )
 
-    $buttonUpDate.on("click",function(){
-        const $form = $(`<form></form>`)
-        const $namePerson = $(`<label>Ingrese al encargado</label>`)
-        const $person = $(`<input type="text" class="form-control" id="marca" name="make" required>`)
-        const $date = $(`<label>Ingrese la fecha</label>`)
-        const $dateDelivery = $(`<input type="date" class="form-control" name="estimateDeliveryDate" id="estimada">`)
-        
-        $form.append($namePerson,$person,$date,$dateDelivery)
-        $articleCont.append($form)
-    })
-    
-    $articleCont.append($make, $model, $id, $km, $dateIn, $descr, $dateOut, $personName, $buttonUpDate)
-    $imgCont.append($image)
-    $rowCont.append($imgCont, $articleCont)
+    $rowCont.append($imgCont, $articleCont, $formCont, $button)
     $colCont.append($rowCont)
 
     $container.append($colCont)
